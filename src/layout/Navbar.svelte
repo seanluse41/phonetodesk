ad3393865c1006a81d38011ed2d656c08927414b
sean <seanluse41@gmail.com>
2020-04-01 12:57:41 +0900

    attempt to fix mobile nav bar

--- src/layout/Navbar.svelte
+++ src/layout/Navbar.svelte
@@ -1,15 +1,23 @@
 <script>
   import { Link } from "svelte-routing";
   import { _ } from "../services/i18n";
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var elems = document.querySelectorAll(".sidenav");
+    var instances = M.Sidenav.init(elems, options);
+  });
 </script>
 
 <nav>
   <div class="nav-wrapper">
+    <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+      <i class="material-icons">menu</i>
+    </a>
     <div class="container">
       <Link to="/">
         <span class="brand-logo">Phone to Desk</span>
       </Link>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <ul id="nav-mobile" class="right hide-on-small-only">
         <li>
           <Link to="/">{$_('navigate-home')}</Link>
         </li>
@@ -20,3 +28,18 @@
     </div>
   </div>
 </nav>
+
+<ul class="sidenav" id="mobile-demo">
+  <li>
+    <a href="sass.html">Sass</a>
+  </li>
+  <li>
+    <a href="badges.html">Components</a>
+  </li>
+  <li>
+    <a href="collapsible.html">Javascript</a>
+  </li>
+  <li>
+    <a href="mobile.html">Mobile</a>
+  </li>
+</ul>
